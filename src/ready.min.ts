54be2cedf5eebce1093374cd99c1d02a3b3de5e6
kzyqq00-Player <1561737625@qq.com>
2024-11-05 07:21:15 +0800

    add window.xxx let the code to readability.

--- src/ready.min.ts
+++ src/ready.min.ts
@@ -28,11 +28,11 @@ $('.left-menu li').on('click', (e) => {
 $('#theme-check').on('change', () =>
     mccg.theme.value = $('#theme-check').val() as sTheme
 );
-addEventListener('hashchange', (e) => { // 合着jq就不行是吧
+window.addEventListener('hashchange', (e) => { // 合着jq就不行是吧
     new URL(e.newURL).hash !== '' && (mccg.showingCmdPage.showing = true);
     mccg.commandPage.call(mccg);
 });
-addEventListener('beforeunload', () =>
+window.addEventListener('beforeunload', () =>
     localStorage['theme'] = mccg.theme.value // 保存主题
 );
 /* $(window).on('beforeunload', (e) => {
