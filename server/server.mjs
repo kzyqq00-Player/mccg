03f40bde337d4be2364a8c56471f52a9694ae85d
kzyqq00-Player <1561737625@qq.com>
2024-12-31 20:32:51 +0800

    McCs的后端搬过来直接用

---
+++ server/server.mjs
@@ -0,0 +1,72 @@
+import { readFile } from 'fs/promises';
+import mime from 'mime';
+import http from 'http';
+import chardet from 'chardet';
+
+const hostname = '127.0.0.1';
+const port = 3000;
+
+/**
+ * @param {http.ServerResponse} res
+ * @param {Error & { code: any }} e
+ */
+function reply500(res, e) {
+    res.statusCode = 500;
+    res.setHeader('Content-Type', 'application/json');
+    res.end(JSONStringifyWithNonEnumerable(Object.assign(e, { '': 'Server Internal Throws Error' })));
+
+    console.error(e);
+}
+
+/**
+ * @param {object} obj
+ */
+function JSONStringifyWithNonEnumerable(obj) {
+    let allProps = {};
+    let keys = Reflect.ownKeys(obj);
+    for (let key of keys) {
+        allProps[key] = obj[key];
+    }
+    return JSON.stringify(allProps);
+}
+
+const server = http.createServer(async (req, res) => {
+    try {
+        const filepath = req.url.slice(1);
+        process.stdout.write(req.method + ' ' + req.url);
+        res.statusCode = 200;
+    
+        if (req.url === '/' || req.url === '') {
+            res.statusCode = 302;
+            res.setHeader('Location', '/index.html');
+            res.end();
+            console.log(' |', res.statusCode);
+            return;
+        }
+
+        res.end(await readFile(filepath)
+            .then(data => {
+                const encoding = chardet.detect(data);
+
+                res.setHeader('Content-Type', mime.getType(filepath).concat(encoding ? '; charset=' + encoding : ''));
+                res.setHeader('Cache-Control', 'no-cache');
+
+                return data;
+            })
+            .catch(e => {
+                if (e.errno = -4058) {
+                    res.statusCode = 404;
+                } else {
+                    reply500(res, e);
+                }
+            }));
+        
+        console.log(' |', res.statusCode);
+    } catch (e) {
+        reply500(res, e);
+    }
+});
+
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
+});
